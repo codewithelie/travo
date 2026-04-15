153f239ee42f931f00d0da08bb1f43359ed1e7ec
codewithelie <me@elyh.fr>
2026-04-15 11:43:33 +0200

    Ajout de la classe Database pour gérer la connexion à la base de données et mise à jour des constantes de configuration

--- config/config.php
+++ config/config.php
@@ -1,2 +1,6 @@
 <?php 
 define('BASE_URL', '/dsp3-o25-f26/travo/public');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'travo');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../app/Core/Router.php';
 require_once __DIR__ . '/../app/Core/Controller.php';
+require_once __DIR__ . '/../app/Core/Database.php';
 
 require_once __DIR__ . '/../app/Models/Project.php';
 
